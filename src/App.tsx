dca8840263d9361afb8cc192045845436ca8d86f
Georgi Aleksandrov <george.a.aleksandrov@gmail.com>
2026-09-16 20:58:52 +0300

    Remove some useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
